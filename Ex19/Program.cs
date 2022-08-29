748bea52463d3a1e7b7ae47f5ffa02d07f5e2843
MAX <maksimdemidovru@mail.ru>
2022-08-29 17:43:47 +0300

    add Ex19

---
+++ Ex19/Program.cs
@@ -0,0 +1,40 @@
+﻿int number = 0;
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    else
+        Console.WriteLine("Неверное число.");
+}
+
+int CheckingPalindromeNumber(int oldValue)
+{
+    oldValue = number;
+    int newValue = 0;
+    while (number > 0)
+    {
+        int dig = number % 10;
+        newValue = newValue * 10 + dig;
+        number = number / 10;
+    }
+    int yes = 0;
+    if (newValue == oldValue)
+    {
+         yes = 1;
+    }
+    else
+    {
+        yes = 0;
+    }
+    return yes;
+}
+int result = CheckingPalindromeNumber(number);
+if (result == 1)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
